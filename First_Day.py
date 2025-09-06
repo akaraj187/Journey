aa903bd89c229c51917eee6d14d3ce6ea35b4e28
akaraj187 <akash_td@DESKTOP-FUGQNF4.localdomain>
2025-09-06 16:36:58 +0000

    tried nano editor

--- First_Day.py
+++ First_Day.py
@@ -1,2 +1,4 @@
 print("Hola,I started Programming with ChatGPT")
 print("Gagan lai Jaado che,MOTU")
+## Okay I have opened this file in wsl nano editor and writing this comment so i will try commiting this to get github updated!!
+print("Ohh!!You learnt how to update and commit using wsl")
