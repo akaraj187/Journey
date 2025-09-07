7527f906ac3bf7090b93aff018d0810045be000b
akaraj187 <akash_td@DESKTOP-FUGQNF4.localdomain>
2025-09-07 09:02:07 +0000

    Loops Basics

---
+++ Loops.py
@@ -0,0 +1,13 @@
+#Python Loops:
+
+count = 1
+print("While Loop")
+while count <=5:
+  print(f"count,{count}")
+  count+=1
+print("For Loop")
+for i in range(1,8):
+   print("you are a beginner")
+print("For Loop")
+for i in range(8):
+   print("you are a learner") 
