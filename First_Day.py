6e3755b12fd9b0d5b830f468c614fc478c6c5a48
akaraj187 <akash_td@DESKTOP-FUGQNF4.localdomain>
2025-09-07 10:40:14 +0000

    Updated First_Day.py with latest changes

--- First_Day.py
+++ First_Day.py
@@ -2,3 +2,14 @@ print("Hola,I started Programming with ChatGPT")
 print("Gagan lai Jaado che,MOTU")
 ## Okay I have opened this file in wsl nano editor and writing this comment so i will try commiting this to get github updated!!
 print("Ohh!!You learnt how to update and commit using wsl")
+count = 1
+print("While Loop")
+while count <=5:
+  print(f"count,{count}")
+  count+=1
+print("For Loop")
+for i in range(1,8):
+   print("you are a learner")
+print("For Loop")
+for i in range(8):
+   print("you are a learner")
