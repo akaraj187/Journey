3b58119dfb594160dba8e6e32e569ae13c461617
akaraj187 <akash_td@DESKTOP-FUGQNF4.localdomain>
2025-09-07 10:24:47 +0000

    Practiced Loops

--- Loops.py
+++ Loops.py
@@ -11,3 +11,30 @@ for i in range(1,8):
 print("For Loop")
 for i in range(8):
    print("you are a learner") 
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+#Using Continue skips the iteration on given condition satisfies
+for i in range(5):
+    if i == 2:
+        break
+    print(i)
+#Using break exits the loops on given condition satisfies
+
+for i in range(1,21):
+    
+    if i%3 == 0:
+        continue
+    print(i)
+    if i == 17:
+        break
+#prints numbers from 1 to 20 skipping multiples of 3 and breaks loop when i=17
+#inner Loops
+
+n=5
+for i in range (n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()   
+#This prints a triangle of stars
